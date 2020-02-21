e7fc7641557a57fa01293cb6e9fed83e17c1172b
jinwoo1225 <jinwoo17962@gmail.com>
2020-02-22 01:16:33 +0900

    added : sample data

---
+++ main.js
@@ -0,0 +1,6 @@
+paths = [
+    new kakao.maps.LatLng(33.452344169439975, 126.56878163224233),
+    new kakao.maps.LatLng(33.452739313807456, 126.5709308145358),
+    new kakao.maps.LatLng(33.45178067090639, 126.5726886938753) 
+]
+lineDrawer(paths);
