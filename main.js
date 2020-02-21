e1105a8ff3adfa510abcd8dba76538cabfd09291
jinwoo1225 <jinwoo17962@gmail.com>
2020-02-21 23:31:51 +0900

    added : control for map api

--- main.js
+++
@@ -1,9 +0,0 @@
-
-//Kakaomap api
-var container = document.getElementById('map');
-var options = {
-    center : new kakao.maps.LatLng(33.450701, 126,570667),
-    level : 3
-}
-
-var map = new kakao.maps.Map(container, options);
