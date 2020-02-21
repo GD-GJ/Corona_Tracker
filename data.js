7bf554544caad26bfd21105a6adb2ceb9273e324
WooSeob Byun <byunws96@gmail.com>
2020-02-22 01:55:14 +0900

    data mod

--- data.js
+++ data.js
@@ -1,7 +1,8 @@
 //각각의 방문한 장소에 대한 정보를 가지는 객체
-function path(time, name, lat, lng){
+function path(time, name, method,lat, lng){
     this.time = time;
     this.name = name;
+    this.method = method;
     this.lat = lat;
     this.lng = lng;
     this.setLatLng = function(lat, lng){
@@ -32,19 +33,19 @@ function person(id, date, hospital, paths){
 
 //테스트 데이터
 var Day1 = new DayPath("2.6")
-Day1.addPath(new path("0930", "대구 동구 소재 회사", 33.452344169439975, 126.56878163224233))
+Day1.addPath(new path("0930", "대구 동구 소재 회사", "자차", 33.452344169439975, 126.56878163224233))
 
 var Day2 = new DayPath("2.7")
-Day2.addPath(new path("1700", "새로난 한방병원", 33.45178067090639, 126.5726886938753))
-Day2.addPath(new path("1701", "자택", 33.452739313807456, 126.5709308145358))
-Day2.addPath(new path("2100", "새로난 한방병원", 33.45178067090639, 126.5726886938753))
+Day2.addPath(new path("1700", "새로난 한방병원", "자차", 33.45178067090639, 126.5726886938753))
+Day2.addPath(new path("1701", "자택", "자차", 33.452739313807456, 126.5709308145358))
+Day2.addPath(new path("2100", "새로난 한방병원", "자차", 33.45178067090639, 126.5726886938753))
 
 var Day3 = new DayPath("2.8")
-Day3.addPath(new path("ALL_DAY", "새로난 한방병원", 33.45178067090639, 126.5726886938753))
+Day3.addPath(new path("ALL_DAY", "새로난 한방병원", "", 33.45178067090639, 126.5726886938753))
 
 var Day4 = new DayPath("2.9")
-Day4.addPath(new path("0730", "신천지예수교증거장막성전 다대오지파대구교회", 33.412739313807456, 126.1009308145358))
-Day4.addPath(new path("0930", "새로난 한방병원", 33.45178067090639, 126.5726886938753))
+Day4.addPath(new path("0730", "신천지예수교증거장막성전 다대오지파대구교회", "자차", 33.412739313807456, 126.1009308145358))
+Day4.addPath(new path("0930", "새로난 한방병원", "자차", 33.45178067090639, 126.5726886938753))
 
 //Day1, 2, 3, 4를 합쳐서
 //확진자가 격리될때 까지의 전체 이동경로 만들기
