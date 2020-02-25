3abdd583e0dc8e4f4fb6293033bd34955eadee71
jinwoo1225 <jinwoo17962@gmail.com>
2020-02-25 19:50:01 +0900

    reverted

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ $("#search_from_myloc").click(function(){
         var lat = position.coords.latitude,
             lng = position.coords.longitude;
 
-        console.log(lat, lng);
+        // console.log(lat, lng);
         
 
         // var locPosition = new kakaoPlaces.maps.LatLng(lat, lon),
